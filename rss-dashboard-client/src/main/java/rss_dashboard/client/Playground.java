e14c8c480443cd8fae4b5c8202f777dd1528a385
moxaj <viktor.magyari92@gmail.com>
2016-11-17 21:56:21 +0100

    Add playgrounds

---
+++ rss-dashboard-client/src/main/java/rss_dashboard/client/Playground.java
@@ -0,0 +1,7 @@
+package rss_dashboard.client;
+
+public class Playground {
+	public static void main(String[] args) {
+		// Start the client
+	}
+}
---
+++ rss-dashboard-server/src/main/java/rss_dashboard/server/Playground.java
@@ -0,0 +1,7 @@
+package rss_dashboard.server;
+
+public class Playground {
+	public static void main(String[] args) {
+		// Start the http server listening on localhost:8080
+	}
+}
