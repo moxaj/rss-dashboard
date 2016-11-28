a9a9cecfb738ce234c7d82f572fbf645f063b17b
LNagyFAttilaC <nagyattila93@hotmail.com>
2016-11-28 22:11:24 +0100

    last update

--- rss-dashboard-client/src/main/java/rss_dashboard/client/controller/LoginController.java
+++ rss-dashboard-client/src/main/java/rss_dashboard/client/controller/LoginController.java
@@ -1,11 +1,8 @@
 package rss_dashboard.client.controller;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.UUID;
 
